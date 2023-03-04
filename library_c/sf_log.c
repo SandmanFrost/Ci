86936f106ed59a456ed9720aae8278098c2d9704
Joaquina Kristina <joaquina@student.21-school.ru>
2023-03-04 22:08:01 +0300

    add_begining_log_library

---
+++ library_c/sf_log.c
@@ -0,0 +1,3 @@
+#include "sf_log.h"
+
+void LogInit(tim *t) { memset(t->stime, 0 , sizeof(t->stime));}
---
+++ library_c/sf_log.h
@@ -0,0 +1,24 @@
+#ifndef _SF_LOG_H_
+#define _SF_LOG_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+typedef struct sf_time {
+    char stime[40];
+} tim;
+
+void LogInit(tim *t);
+void LogTime(tim *t);
+void LogPrint(FILE *f, tim *t, char *filename, char *text);
+void LogPrintNum(FILE *f, tim *t, char *filename, int num);
+void LogPrintChar(FILE *f, tim *t, char *filename, char text);
+void LogStart(char *filename);
+void LogStatus(char *filename, char *status);
+void LogStatusNumber(char *filename, int num);
+void LogStatusChar(char *filename, char status);
+void LogEnd(char *filename);
+
+#endif /*  _SF_LOG_H_  */
