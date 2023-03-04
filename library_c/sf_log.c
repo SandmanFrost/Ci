de6b9c37338aa57c9a97c80d2e0ae50015721e99
Joaquina Kristina <joaquina@student.21-school.ru>
2023-03-04 22:45:57 +0300

    add_log.c

--- library_c/sf_log.c
+++ library_c/sf_log.c
@@ -1,24 +1,67 @@
 #include "sf_log.h"
 
-void LogInit(tim *t) {
-    memset(t->stime, 0 , sizeof(t->stime));
-}
+void LogInit(tim *t) { memset(t->stime, 0, sizeof(t->stime)); }
 
 void LogTime(tim *t) {
-    struct tm *ti;
-    const time_t timer = time(NULL);
-    ti = localetime(&time);
-    strftime(t->stime, sizeof(t->stime), "%Y-%m-%d %H:%M:%S", ti);
+  struct tm *ti;
+  const time_t timer = time(NULL);
+  ti = localetime(&time);
+  strftime(t->stime, sizeof(t->stime), "%Y-%m-%d %H:%M:%S", ti);
 }
 
 void LogPrint(FILE *f, tim *t, char *filename, char *text) {
-    fprintf(f, "[LOG] [TIME %s]\t\t%s: %s\n", t->stime, filename, text);
+  fprintf(f, "[LOG] [TIME %s]\t\t%s: %s\n", t->stime, filename, text);
 }
 
-void LogPrintNum(FILE *f, tim *t, char *filename, int num);
-void LogPrintChar(FILE *f, tim *t, char *filename, char text);
-void LogStart(char *filename);
-void LogStatus(char *filename, char *status);
-void LogStatusNumber(char *filename, int num);
-void LogStatusChar(char *filename, char status);
-void LogEnd(char *filename);
+void LogPrintNum(FILE *f, tim *t, char *filename, int num) {
+  fprintf(f, "[LOG] [TIME %s]\t\t%s: %d\n", t->stime, filename, num);
+}
+
+void LogPrintChar(FILE *f, tim *t, char *filename, char text) {
+  fprintf(f, "[LOG] [TIME %s]\t\t%s: %c\n", t->stime, filename, text);
+}
+
+void LogStart(char *filename) {
+  tim tloc;
+  log_init(&tloc);
+  FILE *f = fopen("LOG.txt", "w");
+  log_time(&tloc);
+  log_print(f, &tloc, filename, "BEGINING PROGRAMM");
+  fclose(f);
+}
+
+void LogStatus(char *filename, char *status) {
+  tim t;
+  log_init(&t);
+  FILE *f = fopen("LOG.txt", "a+");
+  log_time(&t);
+  log_print(f, &t, filename, status);
+  fclose(f);
+}
+
+void LogStatusNumber(char *filename, int num) {
+  tim t;
+  log_init(&t);
+  FILE *f = fopen("LOG.txt", "a+");
+  log_time(&t);
+  log_print_num(f, &t, filename, num);
+  fclose(f);
+}
+
+void LogStatusChar(char *filename, char status) {
+  tim t;
+  log_init(&t);
+  FILE *f = fopen("LOG.txt", "a+");
+  log_time(&t);
+  log_print_char(f, &t, filename, status);
+  fclose(f);
+}
+
+void LogEnd(char *filename) {
+  tim t;
+  log_init(&t);
+  FILE *f = fopen("LOG.txt", "a+");
+  log_time(&t);
+  log_print(f, &t, filename, "END PROGRAMM\n");
+  fclose(f);
+}
