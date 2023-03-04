aa033e4473c246439de98e1a0f442415173215ce
Joaquina Kristina <joaquina@student.21-school.ru>
2023-03-04 22:19:14 +0300

    add_modified_log.c

--- library_c/sf_log.c
+++ library_c/sf_log.c
@@ -1,3 +1,24 @@
 #include "sf_log.h"
 
-void LogInit(tim *t) { memset(t->stime, 0 , sizeof(t->stime));}
+void LogInit(tim *t) {
+    memset(t->stime, 0 , sizeof(t->stime));
+}
+
+void LogTime(tim *t) {
+    struct tm *ti;
+    const time_t timer = time(NULL);
+    ti = localetime(&time);
+    strftime(t->stime, sizeof(t->stime), "%Y-%m-%d %H:%M:%S", ti);
+}
+
+void LogPrint(FILE *f, tim *t, char *filename, char *text) {
+    fprintf(f, "[LOG] [TIME %s]\t\t%s: %s\n", t->stime, filename, text);
+}
+
+void LogPrintNum(FILE *f, tim *t, char *filename, int num);
+void LogPrintChar(FILE *f, tim *t, char *filename, char text);
+void LogStart(char *filename);
+void LogStatus(char *filename, char *status);
+void LogStatusNumber(char *filename, int num);
+void LogStatusChar(char *filename, char status);
+void LogEnd(char *filename);
