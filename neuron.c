060ab6e196d15302f6c6044b9c2776a8e8f12b75
SandmanFrost <90185784+SandmanFrost@users.noreply.github.com>
2023-10-13 16:18:20 +0300

    Update neuron.c

--- neuron.c
+++ neuron.c
@@ -3,132 +3,106 @@
 #include <stdlib.h>
 
 double Sigmoid(double x);
-double Perceptrone(double *x, double **w, double *b, int perc, int now_perc);
-double MSE_loss(double y_true, double y_pred);
-double Deriv_Sigmoid(double x);
+double dSigmoid(double x);
+double Perceptrone(double x1, double w1, double x2, double w2, double b);
+double Ypred_h(double x, double y);
 
 int main() {
-  int out = 1;    // Количество выходных нейронов.
-  int input = 2;  // Количество входных нейронов.
-  int perceptron = 2;  // Количество нейронов скрытого слоя.
-  double *x;           // Массив входных данных.
-  double *h;           // Массив скрытых нейронов.
-  double **w;  // Массив всех весов.
-  double y;    // Выходной нейрон, значение после сигмоиды для применения алгоритма обратного распространения ошибки.
-  double *b;   // Порог вхождения.
-  x = malloc(input * sizeof(double));
-  w = (double **)malloc((input + out) * sizeof(double *));
-  for (int i = 0; i < input + out; i++) {
-    w[i] = (double *)malloc(perceptron * sizeof(double));
-  }
-  b = malloc((perceptron + out) * sizeof(double));
-  h = malloc(perceptron * sizeof(double));
-  for (int i = 0; i < input; i++) {
-    printf("x[%d] = ", i + 1);
-    scanf("%lf", &x[i]);
-  }
-  for (int i = 0; i < input + out; i++) {
-    for (int j = 0; j < perceptron; j++) {
-      if (i == 0) {
-        if (j == 0) w[i][j] = 0;
-        if (j == 1) w[i][j] = 1;
+  double x1, x2, h1, h2, out, w1 = 0.5, w2 = 1, w3 = 2, w4 = -1.5, w5 = 0.5,
+                              w6 = 5, b1 = 10, b2 = -4, b3 = 5, Ytrue;
+  double L_Ypred, Ypred_h1, Ypred_h2, Ypred_w5, Ypred_w6, Ypred_b3;
+  double h1_w1, h1_w2, h1_b1, h2_w3, h2_w4, h2_b2;
+  double sig_h1, sig_h2, sig_out, Lr;
+  int Epoch = 10000000;
+  char c = ' ';
+  Lr = 0.001;
+  for (int i = 0; i <= Epoch; i++) {
+    for (int j = 0; j < 4; j++) {
+      if (j == 0) {
+        x1 = 80 - 72;
+        x2 = 174 - 165;
+        Ytrue = 1;
       }
-      if (i == 1) {
-        if (j == 0) w[i][j] = 1;
-        if (j == 1) w[i][j] = 0;
+      if (j == 1) {
+        x1 = 75 - 72;
+        x2 = 183 - 165;
+        Ytrue = 0;
       }
-      if (i == 2) {
-        if (j == 0) w[i][j] = 0;
-        if (j == 1) w[i][j] = 1;
+      if (j == 0) {
+        x1 = 80 - 72;
+        x2 = 177 - 165;
+        Ytrue = 0; 
       }
+      if (j == 1) {
+        x1 = 72 - 72;
+        x2 = 165 - 165;
+        Ytrue = 1;
+      }
+      h1 = Perceptrone(x1, w1, x2, w2, b1);
+      sig_h1 = Sigmoid(h1);
+      h2 = Perceptrone(x1, w3, x2, w4, b2);
+      sig_h2 = Sigmoid(h2);
+      out = Perceptrone(sig_h1, w5, sig_h2, w6, b3);
+      sig_out = Sigmoid(out);
+      if (i % 1000000 == 0) {
+        printf("Ep=%d #%d: %lf\n", i, j + 1, out);
+      }
+      L_Ypred = -2 * (Ytrue - out);
+      Ypred_h1 = Ypred_h(w5, dSigmoid(sig_out));
+      Ypred_h2 = Ypred_h(w6, dSigmoid(sig_out));
+      Ypred_w5 = Ypred_h(sig_h1, dSigmoid(sig_out));
+      Ypred_w6 = Ypred_h(sig_h2, dSigmoid(sig_out));
+      Ypred_b3 = dSigmoid(sig_out);
+      h1_w1 = Ypred_h(x1, dSigmoid(sig_h1));
+      h1_w2 = Ypred_h(x2, dSigmoid(sig_h1));
+      h1_b1 = dSigmoid(sig_h1);
+      h2_w3 = Ypred_h(x1, dSigmoid(sig_h2));
+      h2_w4 = Ypred_h(x2, dSigmoid(sig_h2));
+      h2_b2 = dSigmoid(sig_h2);
+      w1 = w1 - (Lr * L_Ypred * Ypred_h1 * h1_w1);
+      w2 = w2 - (Lr * L_Ypred * Ypred_h1 * h1_w2);
+      b1 = b1 - (Lr * L_Ypred * Ypred_h1 * h1_b1);
+      w3 = w3 - (Lr * L_Ypred * Ypred_h2 * h2_w3);
+      w4 = w4 - (Lr * L_Ypred * Ypred_h2 * h2_w4);
+      b2 = b2 - (Lr * L_Ypred * Ypred_h2 * h2_b2);
+      w5 = w5 - (Lr * L_Ypred * Ypred_w5);
+      w6 = w6 - (Lr * L_Ypred * Ypred_w6);
+      b3 = b3 - (Lr * L_Ypred * Ypred_b3);
     }
   }
-  for (int i = 0; i < perceptron; i++) {
-    h[i] = Perceptrone(x, w, b, input, i);
-    printf("h[%d]=%.10lf\n", i + 1, h[i]);
+  printf("w1:%lf\nw2:%lf\t\tw5:%lf\nw3:%lf\t\tw6:%lf\nw4:%lf\n", w1, w2, w5, w3,
+         w6, w4);
+  printf("b1=%lf; b2=%lf; b3=%lf\n", b1, b2, b3);
+  while (c != 'q') {
+    printf("x1= ");
+    scanf("%lf", &x1);
+    printf("x2= ");
+    scanf("%lf", &x2);
+    x1 = x1 - 72;
+    x2 = x2 - 165;
+    h1 = Perceptrone(x1, w1, x2, w2, b1);
+    sig_h1 = Sigmoid(h1);
+    h2 = Perceptrone(x1, w3, x2, w4, b2);
+    sig_h2 = Sigmoid(h2);
+    out = Perceptrone(sig_h1, w5, sig_h2, w6, b3);
+    sig_out = Sigmoid(out);
+    if (sig_out >= 0.5) {
+      printf("Woman\n");
+    } else {
+      printf("Man\n");
+    }
+    printf("press Enter for repeat, or 'q' for exit\n");
+    scanf("%c", &c);
   }
-  printf("\n");
-  y = Perceptrone(h, w, b, perceptron, input + out - 1);
-  free(x);
-  free(h);
-  free(w);
-  free(b);
-  printf("out = %.10lf\n", y);
   return 0;
 }
 
-double Sigmoid(double x) {
-  x = 1 / (1 + exp(-1 * x));
-  return x;
+double Sigmoid(double x) { return 1 / (1 + exp(-x)); }
+double dSigmoid(double x) { return x * (1 - x); }
+double Perceptrone(double x1, double w1, double x2, double w2, double b) {
+  return w1 * x1 + w2 * x2 + b;
 }
-
-double Perceptrone(double *x, double **w, double *b, int perc, int now_perc) {
-  double q = 0;
-  double n[perc];
-  for (int i = 0; i < perc; i++) {
-    n[perc] = x[i] * w[now_perc][i];
-    q = q + n[perc];
-  }
-  q = b[now_perc];
-  return Sigmoid(q);
-}
-
-double MSE_loss(double y_true, double y_pred) {
-  double g = 0;
-  g = pow(y_true - y_pred, 2);
-  return g;
-}
-
-double Deriv_Sigmoid(double x) {
-  double fx = Sigmoid(x);
-  return fx * (1 - fx);
-}
-
-double Train(double **w, double *x, double *h, double *b, double out) {
-  double Learn_rate = 0.01;
-  int epoch = 100000;
-  for (int i = 0; i < epoch; i++) {
-    double sum_out = w[2][0] * h[0] + w[2][1] * h[1] + b[2];
-    double output = Sigmoid(sum_out);
-    double sum_h1 = w[0][0] * x[0] + w[0][1] * x[1] + b[0];
-    double h1 = Sigmoid(sum_h1);
-    double sum_h2 = w[1][0] * x[0] + w[1][1] * x[1] + b[1];
-    double h2 = Sigmoid(sum_h2);
-
-    double L_yp = -2 * (1 - out);
-
-    double yp_w5 = h[0] * Deriv_Sigmoid(sum_out);
-    double yp_w6 = h[1] * Deriv_Sigmoid(sum_out);
-    double yp_b3 = Deriv_Sigmoid(sum_out);
-    double yp_h1 = w[2][0] * Deriv_Sigmoid(sum_out);
-    double yp_h2 = w[2][1] * Deriv_Sigmoid(sum_out);
-
-    double h1_w1 = x[0] * Deriv_Sigmoid(sum_h1);
-    double h1_w2 = x[1] * Deriv_Sigmoid(sum_h1);
-    double h1_b1 = Deriv_Sigmoid(sum_h1);
-
-    double h2_w3 = x[0] * Deriv_Sigmoid(sum_h2);
-    double h2_w4 = x[1] * Deriv_Sigmoid(sum_h2);
-    double h2_b2 = Deriv_Sigmoid(sum_h2);
-
-    w[0][0] = w[0][0] - (Learn_rate * L_yp * yp_h1 * h1_w1);
-    w[0][1] = w[0][1] - (Learn_rate * L_yp * yp_h1 * h1_w2);
-    b[0] = b[0] - (Learn_rate * L_yp * yp_h1 * h1_b1);
-
-    w[1][0] = w[1][0] - (Learn_rate * L_yp * yp_h2 * h2_w3);
-    w[1][1] = w[1][1] - (Learn_rate * L_yp * yp_h2 * h2_w4);
-    b[1] = b[1] - (Learn_rate * L_yp * yp_h2 * h2_b2);
-
-    w[2][0] = w[2][0] - (Learn_rate * L_yp * yp_w5);
-    w[2][1] = w[2][1] - (Learn_rate * L_yp * yp_w6);
-    b[2] = b[2] - (Learn_rate * L_yp * yp_b3);
-    // if (epoch % 10 == 0) {
-    //     double Loss;
-    //     Loss = MSE_loss(,out)
-    // }
-  }
-}
-
+double Ypred_h(double x, double y) { return x * y; }
 // dL/dy_pred = -2 * (1 - y_pred);
 // dy_pred/dh1 = w5 * F'(w5 * h1 + w6 * h2 + b3);
 // dh1/dw1 = x1 * F'(w1 * x1 + w2 * x2 + b1);
